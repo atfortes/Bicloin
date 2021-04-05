29db24393606295973468c13acfc233a6c82380d
diogosoares22 <diogo.sousa.soares2000@gmail.com>
2021-04-05 16:12:51 +0100

    added hub frontend

---
+++ hub-tester/src/main/java/pt/tecnico/bicloin/hub/HubFrontend.java
@@ -0,0 +1,47 @@
+package pt.tecnico.bicloin.hub;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import pt.tecnico.bicloin.hub.grpc.*;
+import pt.tecnico.bicloin.hub.grpc.HubServiceGrpc;
+import pt.ulisboa.tecnico.sdis.zk.*;
+
+public class HubFrontend implements AutoCloseable {
+
+    final ManagedChannel channel;
+    HubServiceGrpc.HubServiceBlockingStub stub;
+
+    public HubFrontend(String zooHost, int zooPort, String path) throws ZKNamingException {
+        ZKNaming zkNaming = new ZKNaming(zooHost, String.valueOf(zooPort));
+        ZKRecord hub = zkNaming.lookup(path);
+        String target = hub.getURI();
+        channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        stub = HubServiceGrpc.newBlockingStub(channel);
+    }
+
+    public BalanceResponse balance(BalanceRequest request) { return stub.balance(request); }
+
+    public BikeResponse bikeUp(BikeRequest request) { return stub.bikeUp(request); }
+
+    public BikeResponse bikeDown(BikeRequest request) {return stub.bikeDown(request);}
+
+    public InfoStationResponse infoStation(InfoStationRequest request) { return  stub.infoStation(request);}
+
+    public LocateStationResponse locateStation(LocateStationRequest request) { return stub.locateStation(request);}
+
+    public CtrlPingResponse ctrlPing(CtrlPingRequest request) {
+        return stub.ctrlPing(request);
+    }
+
+    public SysStatusResponse sysStatus(SysStatusRequest request) {
+        return stub.sysStatus(request);
+    }
+
+    public TopUpResponse topUp(TopUpRequest request) {return stub.topUp(request);}
+
+
+    @Override
+    public final void close() {
+        channel.shutdown();
+    }
+}
