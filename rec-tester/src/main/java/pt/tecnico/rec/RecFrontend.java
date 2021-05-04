2b483819b272ca4c6082aefc3e34dd2ba9dcd07b
atfortes <altf@netcabo.pt>
2021-05-04 15:01:23 +0100

    implemented write back and readResponse comparator

--- rec-tester/src/main/java/pt/tecnico/rec/RecFrontend.java
+++ rec-tester/src/main/java/pt/tecnico/rec/RecFrontend.java
@@ -7,6 +7,7 @@ import pt.tecnico.rec.grpc.RecordServiceGrpc;
 import pt.ulisboa.tecnico.sdis.zk.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -25,7 +26,7 @@ public class RecFrontend implements AutoCloseable {
     List<Double> weights = new ArrayList<>();
     ZKNaming zk;
     int cid;
-    double maxWeight = 1;
+    double maxWeight;
     long WAIT_TIME = 5000;
     private ConcurrentHashMap<String, Long> stats = new ConcurrentHashMap<>();
 
@@ -46,13 +47,13 @@ public class RecFrontend implements AutoCloseable {
         this.cid = cid;
     }
 
-    // FIXME empty responses and exception handling
+    // FIXME empty responses
     public Rec.CtrlPingResponse ctrlPing(Rec.CtrlPingRequest request) {
 
         stats.merge(PING_COUNTER_KEY, 1L, Long::sum);
         long start = System.currentTimeMillis();
 
-        ResponseCollector<Rec.CtrlPingResponse> collector = new ResponseCollector<>(maxWeight);
+        ResponseCollector<Rec.CtrlPingResponse> collector = new ResponseCollector<>(1);
 
         SendRequestToAll<Rec.CtrlPingRequest, Rec.CtrlPingResponse> sender =
                 new SendRequestToAll<>(request, collector, ((stub, req, observer) -> stub.ctrlPing(req, observer)));
@@ -62,7 +63,7 @@ public class RecFrontend implements AutoCloseable {
             try {
                 collector.wait(WAIT_TIME);
                 var responses = collector.getResponses();
-                if (responses.size() == 0) { return null; } // FIXME should emit error quorum
+                if (responses.size() == 0) { return null; }
 
                 stats.merge(PING_TIMER_KEY, System.currentTimeMillis()-start, Long::sum);
                 return responses.get(0);
@@ -74,7 +75,7 @@ public class RecFrontend implements AutoCloseable {
         return null;
     }
 
-    // FIXME exception handling
+    // FIXME quorum accomplished verification and exception handling
     public Rec.ReadResponse read(Rec.ReadRequest request) {
 
         stats.merge(READ_COUNTER_KEY, 1L, Long::sum);
@@ -84,14 +85,28 @@ public class RecFrontend implements AutoCloseable {
 
         SendRequestToAll<Rec.ReadRequest, Rec.ReadResponse> sender =
                 new SendRequestToAll<>(request, collector, ((stub, req, observer) -> stub.read(req, observer)));
-        
+
         synchronized(collector) {
             sender.run();
             try {
                 collector.wait(WAIT_TIME);
 
+                Rec.ReadResponse response = Collections.max(collector.getResponses(), new Comparator<Rec.ReadResponse>() {
+                    @Override
+                    public int compare(Rec.ReadResponse first, Rec.ReadResponse second) {
+                        int seqComparison = Integer.compare(first.getSeq(), second.getSeq());
+                        if (seqComparison == 0) return Integer.compare(first.getCid(), second.getCid());
+                        else return seqComparison;
+                    }
+                });
+
+                // write back
+                Rec.WriteRequest writeBackRequest = Rec.WriteRequest.newBuilder().setName(request.getName())
+                        .setSeq(response.getSeq()).setCid(response.getCid()).setValue(response.getValue()).build();
+                this.write(writeBackRequest);
+
                 stats.merge(READ_TIMER_KEY, System.currentTimeMillis()-start, Long::sum);
-                return collector.getResponses().stream().max(Comparator.comparing(el -> el.getSeq()*10 + el.getCid())).get();
+                return response;
             }
             catch (InterruptedException e){
                 System.err.println("Caught exception: " + e.toString());
@@ -100,7 +115,7 @@ public class RecFrontend implements AutoCloseable {
         return null;
     }
 
-    // FIXME empty responses and exception handling
+    // FIXME quorum accomplished verification and exception handling
     public Rec.WriteResponse write(Rec.WriteRequest request) {
 
         stats.merge(WRITE_COUNTER_KEY, 1L, Long::sum);
@@ -122,7 +137,7 @@ public class RecFrontend implements AutoCloseable {
             try {
                 collector.wait(WAIT_TIME);
                 var responses = collector.getResponses();
-                if (responses.size() == 0) { return null; } // FIXME should emit error quorum
+                if (responses.size() == 0) { return null; }
 
                 stats.merge(WRITE_TIMER_KEY, System.currentTimeMillis()-start, Long::sum);
                 return responses.get(0);
